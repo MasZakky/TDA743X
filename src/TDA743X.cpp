3d50964dddecb8c67c5115d4759b09e98eaac03c
Mas Zakky <44503482+MasZakky@users.noreply.github.com>
2019-04-17 14:57:12 +0700

    .
    .

--- src/TDA743X.cpp
+++ src/TDA743X.cpp
@@ -1,22 +1,19 @@
 #include <TDA743X.h>
 
-int8_t TDA7430::begin()             //
-{
+int8_t TDA7430::begin(){
   return TDA7430::begin(AddrTDA7430,0x00);
 }
-int8_t TDA7430::begin(byte Address) //Address  
-{
+
+int8_t TDA7430::begin(byte Address){
   return TDA7430::begin(Address,0x00);
 }
-int8_t TDA7430::begin(byte Address,  //Address
-                      byte Addr)     //Channel
-{
+
+int8_t TDA7430::begin(byte Address,byte Addr){
   _Address = Address | (Addr<<1);
   return Process_OK;
 }
 
-int8_t TDA7430::Input(int8_t val)   //Input
-{
+int8_t TDA7430::Input(int8_t val){
   if(_MInput ==  1){
     _DATA[9]  &= ~(0x03);
     return Process_OK;
@@ -31,16 +28,15 @@ int8_t TDA7430::Input(int8_t val)   //Input
   return TDA7430::setWrite(9,_DATA[9]);
 }
 
-int8_t TDA7430::Volume(int8_t val)  //Volume
-{
-  val = constrain(val,0,63);
+int8_t TDA7430::Volume(int8_t val){
+  val = Constrain(val,0,63);
   if((int8_t)(_DATA[0]&0x3F) == val) return Process_OK;
   
    _DATA[0] = (_DATA[0]&(~(0x3F))) | val;
   return TDA7430::setWrite(0,_DATA[0]);
 }
-int8_t TDA7430::Bass(int8_t val)    //Bass
-{
+
+int8_t TDA7430::Bass(int8_t val){
   val = constrain(val,-7,7);
   if(val < 0)  val = val  + 7;
   else val = 8 + (7 - val);
@@ -50,8 +46,8 @@ int8_t TDA7430::Bass(int8_t val)    //Bass
   _DATA[3] = (_DATA[3]&(~(0x0F))) |  val);
   return TDA7430::setWrite(3,_DATA[3]);
 }
-int8_t TDA7430::Middle(int8_t val)  //Middle
-{
+
+int8_t TDA7430::Middle(int8_t val){
   val = constrain(val,-7,7);
   if(val < 0)  val = val  + 7;
   else val = 8 + (7 - val);
@@ -60,8 +56,8 @@ int8_t TDA7430::Middle(int8_t val)  //Middle
   _DATA[4] = (_DATA[4]&(~(0x0F))) |  val);
   return TDA7430::setWrite(4,_DATA[4]);
 }
-int8_t TDA7430::Treble(int8_t val)  //Treble
-{
+
+int8_t TDA7430::Treble(int8_t val){
   val = constrain(val,-7,7);
   if(val < 0)  val = val  + 7;
   else val = 8 + (7 - val);
@@ -71,16 +67,16 @@ int8_t TDA7430::Treble(int8_t val)  //Treble
   return TDA7430::setWrite(4,_DATA[4]);
 }
 
-int8_t TDA7430::Balance(int8_t val)                //Value
-{
+int8_t TDA7430::Balance(int8_t val){
   TDA7430::Balance(val,ATT_LR);
   TDA7430::Balance(val,ATT_RR);
   TDA7430::Balance(val,ATT_LF);
   return TDA7430::Balance(val,ATT_RF);
 }
-int8_t TDA7430::Balance(int8_t val,int8_t val2)   //Value,channel
-{
-  val = constrain(val,0,79);
+
+int8_t TDA7430::Balance(int8_t val,int8_t val2){
+  val = Constrain(val,0,79);
+  val2 = constrain(val2,0,3);
   switch(val2){
     case ATT_LF: if(_DATA[5] != val)return TDA7430::setWrite(5,(_DATA[5]= val)); break;
     case ATT_RF: if(_DATA[6] != val)return TDA7430::setWrite(6,(_DATA[6]= val)); break; 
@@ -90,70 +86,144 @@ int8_t TDA7430::Balance(int8_t val,int8_t val2)   //Value,channel
   return Process_OK;
 }
 
-int8_t TDA7430::NaturalBase(int8_t val) //NaturalBase
-{
+int8_t TDA7430::NaturalBase(int8_t val){
   val = constrain(val,0,1);
   if(((_DATA[3]>>4)&0x01) == val)  return Process_OK;
 
-  _DATA[3] = (_DATA[3]&(~(0x01<<4))) | val<<4;
+  _DATA[3] = (_DATA[3]&(~(0x01<<4))) | (val<<4);
   return TDA7430::setWrite(3,_DATA[3]);
 }
-int8_t TDA7430::EnableNaturalBase()     //Enable
-{
-  return TDA7430::NaturalBase(0);
-}
-int8_t TDA7430::DisableNaturalBase()    //Disable
-{
-  return TDA7430::NaturalBase(1);
+
+int8_t TDA7430::EnableNaturalBase(){
+  return TDA7430::NaturalBase(NATURALBASE_ACTIVE);
 }
 
-int8_t TDA7430::RearSwitch(int8_t val)  //Rear
-{
+int8_t TDA7430::DisableNaturalBase(){
+  return TDA7430::NaturalBase(NATURALBASE_OFF);
+}
+
+int8_t TDA7430::RearSwitch(int8_t val){
   val = constrain(val,0,1);
   if(((_DATA[0]>>6)&0x01) == val)  return Process_OK;
 
   _DATA[0] = (_DATA[0]&(~(0x01<<6))) | val<<6;
   return TDA7430::setWrite(0,_DATA[0]);  
 }
-int8_t TDA7430::EnableRear()            //Enable
-{
-  return TDA7430::RearSwitch(0);
-}
-int8_t TDA7430::DisableRear()           //Disable
-{
-  return TDA7430::RearSwitch(1);
+
+int8_t TDA7430::EnableRear(){
+  return TDA7430::RearSwitch(REAR_ACTIVE);
 }
 
-int8_t TDA7430::SurroundMode(int8_t val);  //Surround Mode
-int8_t TDA7430::Surround_SIMULATED();      //SIMULATED
-int8_t TDA7430::Surround_MUSIC();          //MUSIC
-int8_t TDA7430::Surround_MOVIE();          //MOVIE
-int8_t TDA7430::Surround_OFF();            //OFF
+int8_t TDA7430::DisableRear(){
+  return TDA7430::RearSwitch(REAR_OFF);
+}
 
-int8_t TDA7430::EffectControl(int8_t val); //EffectControl
+int8_t TDA7430::SurroundMode(int8_t val){
+  val = cosntrain(val,0,3);
+  if((_DATA[1]&0x03== val) return Process_OK;
+  
+  _DATA[1] = (_DATA[1]&(~(0x07))) | val;
+  return TDA7430::setWrite(1,_DATA[1]);
+}
 
-int8_t TDA7430::PhaseResistor(int8_t val);     // 0 -  255
-int8_t TDA7430::PhaseResistor(int8_t val,      // 0 - 3
-                              int8_t val1,     // 0 - 3
-                              int8_t val2,     // 0 - 3
-                              int8_t val3);    // 0 - 3
-int8_t TDA7430::PhaseResistor1(int8_t val);    // 0 - 3
-int8_t TDA7430::PhaseResistor2(int8_t val);    // 0 - 3
-int8_t TDA7430::PhaseResistor3(int8_t val);    // 0 - 3
-int8_t TDA7430::PhaseResistor4(int8_t val);    // 0 - 3
+int8_t TDA7430::Surround_SIMULATED(){
+  return TDA7430::SurroundMode(Surround_SIMULATED);
+}
 
-int8_t TDA7430::VoiceCanceller(int8_t val);    //VoiceCanceler
-int8_t TDA7430::DisableVoiceCanceller():       //Disable
-int8_t TDA7430::EnableVoiceCanceller();        //Input
+int8_t TDA7430::Surround_MUSIC(){
+  return TDA7430::SurroundMode(Surround_MUSIC);
+}
 
-int8_t TDA7430::SelectorRecOut(int8_t val);    // value
-int8_t TDA7430::SelectorRecOut(int8_t val,     // Value
-                               int8_t val2);   // Channel
-int8_t TDA7430::RecOut_3BAND();                // 3Band
-int8_t TDA7430::RecOut_SURR();                 // Surr
-int8_t TDA7430::RecOut_REAR();                 // Rear
-int8_t TDA7430::RecOut_OFF();                  // Off
+int8_t TDA7430::Surround_MOVIE(){
+  return TDA7430::SurroundMode(Surround_MOVIE);
+}
 
+int8_t TDA7430::Surround_OFF(){
+  return TDA7430::SurroundMode(Surround_OFF);
+}
+
+int8_t TDA7430::EffectControl(int8_t val){
+  val = constrain(val,-21,-6);
+  val = (val*-1) - 6;
+  if(((_DATA[1]>>3)&0x07)) == val) return Process_OK;
+  
+  _DATA[1] = (_DATA[1]&(~(0x07))) | (val<<3);
+  return TDA7430::setWrite(1,_DATA[1]);
+}
+
+int8_t TDA7430::PhaseResistor(int8_t val){
+  val = constrain(val,0,255);
+  if(_DATA[2] == val) return Process_OK;
+  
+  _DATA[2] = val;
+  return TDA7430::setWrite(2,_DATA[2]);
+}
+
+int8_t TDA7430::PhaseResistor(int8_t val,int8_t val1,int8_t val2,int8_t val3){
+ val = constrain(val,0,3);
+ val1= constrain(val1,0,3);
+ val2= constrain(val2,0,3);
+ val3= constrain(val3,0,3);
+ return  TDA7430::PhaseResistor(val | (val1<<2) | (val2<<4) | (val3<<6));
+}
+
+// int8_t TDA7430::PhaseResistor1(int8_t val);    // 0 - 3
+// int8_t TDA7430::PhaseResistor2(int8_t val);    // 0 - 3
+// int8_t TDA7430::PhaseResistor3(int8_t val);    // 0 - 3
+// int8_t TDA7430::PhaseResistor4(int8_t val);    // 0 - 3
+
+int8_t TDA7430::VoiceCanceller(int8_t val){
+  val = constrain(val,0,1);
+  if((_DATA[9]&0x01) == val) return Process_OK;
+  
+  _DATA[9] = (_DATA[9]&(~(0x01))) | val;
+  return TDA7430::setWrite(9,_DATA[9]);
+}
+
+int8_t TDA7430::DisableVoiceCanceller(){
+  return TDA7430::VoiceCanceller(VOICECANCELLER_OFF);
+}
+
+int8_t TDA7430::EnableVoiceCanceller(){
+  return TDA7430::VoiceCanceller(VOICECANCELLER_ON);
+}
+
+int8_t TDA7430::SelectorRecOut(int8_t val){
+  TDA7430::SelectorRecOut(val,RecOut_L);
+  return TDA7430::SelectorRecOut(val,RecOut_R);
+}
+
+int8_t TDA7430::SelectorRecOut(int8_t val,int8_t val2){
+  val = constrain(val,0,3);
+  val2 = constrain(val2,0,1);
+  switch(val2){
+    case RecOut_L: if(((_DATA[9]>>3)&0x03) != val) _DATA[9] = (_DATA[9]&(~(0x03<<3))) | (val << 3);
+                   return TDA7430::setWrite(9,_DATA[9]); break;
+    case RecOut_R: if(((_DATA[9]>>5)&0x03) != val) _DATA[9] = (_DATA[9]&(~(0x03<<5))) | (val << 5);
+                   return TDA7430::setWrite(9,_DATA[9]); break;
+  }
+  return Process_OK;
+}
+
+int8_t TDA7430::RecOut_3BAND(){
+  return TDA7430::SelectorRecOut(Out_3BAND);
+}
+
+int8_t TDA7430::RecOut_SURR(){
+  return TDA7430::SelectorRecOut(Out_SURR);
+}
+
+int8_t TDA7430::RecOut_REAR(){
+  return TDA7430::SelectorRecOut(Out_REAR);
+}
+
+int8_t TDA7430::RecOut_OFF(){
+  return TDA7430::SelectorRecOut(Out_FLAT);
+}
+
+int8_t RecOut_FLAT(){
+  return TDA7430::RecOut_OFF();
+}
 
 int8_t UseValue(int8_t val){
   _UPN = val;
