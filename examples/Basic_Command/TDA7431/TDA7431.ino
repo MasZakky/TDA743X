662ed35df001913053fbd84a8b3300bc8c39ca42
Mas Zakky <44503482+MasZakky@users.noreply.github.com>
2019-04-22 07:53:07 +0700

    Update

--- examples/Basic_Command/TDA7431/TDA7431.ino
+++ examples/Basic_Command/TDA7431/TDA7431.ino
@@ -6,41 +6,45 @@ void setup() {
   Wire.begin();
   uPA.begin();
 //  uPA.begin(0x80);  // Address
+//  uPA.begin(0x80,1);
   
-  uPA.InstalWire(&Wire);      // Instal Wire
+uPA.InstalWire(&Wire);      // Instal Wire
 //  uPA.InstalWire(&Wire1);   // Instal Wire1
 //  uPA.InstalWire(&Wire2);   // Instal Wire2
 
-  uPA.UseValue(Use_POSITIVE);   //UsePOSITIVE
+uPA.UseValue(Use_POSITIVE);   //UsePOSITIVE
 //  uPA.UseValue(Use_NEGATIVE);   //UseNEGATIVE
 
 //  uPA.Input(1);       // FIX
 
 uPA.Volume(0);      // 0  dB (1dB Step)
-  // UseValue == Use_POSITIVE => 0 ~ 63  => 0 dB ~ -63dB
-  // UseValue == Use_NEGATIVE => -63 ~0  => 0 dB ~ -63dB
+  // UserValue == Use_POSITIVE => Volume 0 ~ 63  => 0dB ~ -31.5dB (0.5dB Step)
+  // UserValue == Use_NEGATIVE => Volume -63 ~ 0 => -31.5dB ~ 0dB (0.5dB Step)
   
-  uPA.Bass(7);        // 14 dB 
+uPA.Bass(7);        // 14 dB 
   // Value -7 ~ 7  => -14dB ~ 14dB (2dB Step)
   
-  uPA.Middle(7);      // 14 dB
+uPA.Middle(7);      // 14 dB
   // Value -7 ~ 7  => -14dB ~ 14dB (2dB Step);
   
-  uPA.Treble(7);      // 14 dB
+uPA.Treble(7);      // 14 dB
   // Value -7 ~ 7  => -14dB ~ 14dB (2dB Step);
 
-  uPA.Balance(0);         // 0 dB ALL OUTPUT (1dB Step)
+uPA.Balance(0);         // 0 dB ALL OUTPUT (1dB Step)
 //  uPA.Balance(0,ATT_LR);  // 0 dB Output ATT_LR
 //  uPA.Balance(0,ATT_RR);  // 0 dB Output ATT_RR
 //  uPA.Balance(0,ATT_LF);  // 0 dB Output ATT_LF
 //  uPA.Balance(0,ATT_RF);  // 0 dB Output ATT_RF
-
-  uPA.NaturalBase(NATURALBASE_ACTIVE);  // NATURALBASE_ACTIVE
+  // UserValue == Use_POSITIVE => Balance 0 ~ 79  => 0dB ~ -79dB (1dB Step)
+  // UserValue == Use_NEGATIVE => Balance -79 ~ 0 => -79dB ~ 0dB (1dB Step)
+  // Channel => ATT_LR,ATT_RR,ATT_LF, or ATT_RF 
+  
+uPA.NaturalBase(NATURALBASE_ACTIVE);  // NATURALBASE_ACTIVE
 //  uPA.NaturalBase(NATURALBASE_OFF);   // NATURALBASE_OFF
 //  uPA.EnableNaturalBase();            // Enable  or Active
 //  uPA.DisableNaturalBase();           // Disable or Off
 
-  uPA.SelectorRecOut(Out_3BAND);          // Channel ATT_LR & ATT_RR  
+uPA.SelectorRecOut(Out_3BAND);          // Channel ATT_LR & ATT_RR  
 //  uPA.SelectorRecOut(Out_SURR);           // Channel ATT_LR & ATT_RR  
 //  uPA.SelectorRecOut(Out_REAR);           // Channel ATT_LR & ATT_RR  
 //  uPA.SelectorRecOut(Out_OFF);            // Channel ATT_LR & ATT_RR   
@@ -73,7 +77,8 @@ uPA.SurroundMode(Surr_OFF);
 // uPA.Surround_SIMULATED();    
 // uPA.Surround_MUSIC();         
 // uPA.Surround_MOVIE();        
-// uPA.Surround_OFF();          
+// uPA.Surround_OFF();     
+  // Value   => Surr_SIMULATED,Surr_MUSIC,Surr_MOVIE, or Surr_OFF 
 
 uPA.EffectControl(-6); // -6 ~ -21
 
@@ -97,6 +102,8 @@ uPA.VoiceCanceller(VOICECANCELLER_OFF);
 //  uPA.Mute(MUTE_ON, ATT_RR);    // MUTE_ON  ,ATT_RF
 //  uPA.DisableMute();        // Disable
 //  uPA.EnableMute();         // Enable
+  // Value   => MUTE_OFF or MUTE_ON
+  // Channel => ATT_LR,ATT_RR,ATT_LF, or ATT_RF
 
 }
 
